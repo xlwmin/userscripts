91a0d1edf8c3229f1ed77943216752ba50944a92
xlwmin <xlwmin@outlook.com>
2017-09-30 16:26:20 +0800

    创建userscripts

---
+++ fashion.drama.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         细声视频播放：优酷，爱奇艺，腾讯，搜狐剧集
+// @namespace    http://drama.xlwmin.pub
+// @version      1.1.3
+// @description  细声视频播放，VIP视频播放,修正腾讯集数预告不纳入计算
+// @author       细声工作室
+// @match        *://v.youku.com/v_show/id_*.html*
+// @match        *://www.iqiyi.com/v_*.html*
+// @match        *://tv.sohu.com/*/n*.shtml*
+// @match        *://v.qq.com/x/cover/*/*.html*
+// @match        *://v.qq.com/x/cover/*.html*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+
+
+Page = {
+    entry:function(){
+        //创建按钮
+        var text = document.createTextNode("▶");
+
+        //创建按钮
+        var div = document.createElement("div");
+        div.setAttribute("title","进入细声视频播放。");
+        div.setAttribute("class","whisper-drama");
+
+        //注册事件
+        div.addEventListener("click",this.onClick);
+
+        //添加到页面
+        div.appendChild(text);
+        document.body.appendChild(div);
+    },
+    onClick:function(){
+        window.open("http://drama.xlwmin.pub?link="+window.location.href);
+    }
+};
+(function() {
+    'use strict';
+    GM_addStyle(".whisper-drama{position:fixed;left:0;top:185px;color:#fff;z-index:9999;width:18px;line-height:50px;text-align:center;border-radius:0px 5px 5px 0px;background:#666;cursor:default;}.whisper-drama:hover{color:white;background:#880000;cursor:pointer;}");
+
+    Page.entry();
+})();
