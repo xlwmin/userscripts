2b958dd2b729eb094455c456e742b1fe30ac932f
xlwmin <xlwmin@outlook.com>
2018-01-18 15:37:33 +0800

    增加fashion.dramas.user.js脚本

---
+++ fashion.dramas.user.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name         细声影视收藏
+// @namespace    http://dramas.xlwmin.pub
+// @version      1.0.0
+// @description  细声影视收藏
+// @author       细声工作室
+// @match        *://v.youku.com/v_show/id_*.html*
+// @match        *://www.iqiyi.com/v_*.html*
+// @match        *://tv.sohu.com/*/n*.shtml*
+// @match        *://film.sohu.com/album/*.html*
+// @match        *://v.qq.com/x/cover/*/*.html*
+// @match        *://v.qq.com/x/cover/*.html*
+// @match        *://www.mgtv.com/b/*/*.html*
+// @match        *://v.pptv.com/show/*.html*
+// @match        *://www.le.com/ptv/vplay/*.html*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+
+Page = {
+    entry:function(){
+        //创建按钮
+        var text = document.createTextNode("★");
+
+        //创建按钮
+        var div = document.createElement("div");
+        div.setAttribute("title","收藏");
+        div.setAttribute("class","whisper-dramas");
+
+        //注册事件
+        div.addEventListener("click",this.onClick);
+
+        //添加到页面
+        div.appendChild(text);
+        document.body.appendChild(div);
+    },
+    onClick:function(){
+        var form = document.createElement("form");
+        form.setAttribute("method","POST");
+        form.setAttribute("action","http://dramas.xlwmin.pub")
+        form.setAttribute("target","_blank");
+        form.style.display="none";
+
+        var inputText = document.createElement("input");
+        inputText.setAttribute("name","link");
+        inputText.setAttribute("type","hidden");
+        inputText.setAttribute("value",window.location.href);
+        form.appendChild(inputText);
+
+        document.body.appendChild(form);
+        form.submit();
+    }
+};
+(function() {
+    'use strict';
+    GM_addStyle(".whisper-dramas{position:fixed;left:0;bottom:5px;color:#fff;z-index:9999;width:18px;line-height:50px;text-align:center;border-radius:0px 5px 5px 0px;background:#666;cursor:default;font-size:14px;}.whisper-dramas:hover{color:white;background:#880000;cursor:pointer;}");
+
+    Page.entry();
+})();
