5d6af7a8f607d26d2ac200910eb6e32f65fd6e3e
xlwmin <xlwmin@outlook.com>
2019-02-12 10:34:01 +0800

    修复百度翻译内容

--- plugin.translate.baidu.js
+++ plugin.translate.baidu.js
@@ -1,52 +1,58 @@
 // ==UserScript==
-// @name         细声一直播全屏
-// @namespace    http://xlwmin.pub
-// @version      1.0
-// @description  一直播全屏
+// @name         细声百度翻译
+// @namespace    http://www.xlwmin.pub
+// @version      1.0.0
+// @description  选中文字跳转百度翻译
 // @author       细声工作室
 // @license      MIT
-// @match        *://www.yizhibo.com/l/*.html
+// @match        *://*.quora.*
+// @match        *://*.youtube.com/watch*
 // @grant        GM_addStyle
 // ==/UserScript==
 
 
 //测试同步
 var Page = {
+    text:null,
     entry:function(){
         //创建按钮
-        var text = document.createTextNode("▣");
+        var text = document.createTextNode("zh");
 
         //创建按钮
         var div = document.createElement("div");
-        div.setAttribute("title","全屏播放");
-        div.setAttribute("class","whisper-yizhibo");
+        div.setAttribute("title","跳转翻译");
+        div.setAttribute("class","whisper-translate");
 
         //注册事件
         div.addEventListener("click",this.onClick);
 
+        window.addEventListener("mouseup",this.onWindowMouseUp);
+
         //添加到页面
         div.appendChild(text);
-
-        var contrl = document.getElementById('J_contrl');
-        contrl.appendChild(div);
+        document.body.appendChild(div);
+    },
+    onWindowMouseUp:function(){
+        var text = window.getSelection().toString();
+        if(text!=''){
+            Page.text = text;
+        }
     },
     onClick:function(){
-        var video = document.getElementById('J_flash');
-        var element = video;
-        if (element.requestFullscreen) {
-            element.requestFullscreen();
-        } else if (element.mozRequestFullScreen) {
-            element.mozRequestFullScreen();
-        } else if (element.msRequestFullscreen) {
-            element.msRequestFullscreen();
-        } else if (element.webkitRequestFullscreen) {
-            element.webkitRequestFullScreen();
-        }
+        var form = document.createElement("form");
+        form.setAttribute("method","get");
+        form.setAttribute("action","https://fanyi.baidu.com/#en/zh/"+Page.text);
+        form.setAttribute("target","_blank");
+        form.style.display="none";
+
+        document.body.appendChild(form);
+        //console.log(Page.text);
+        form.submit();
     }
 };
 (function() {
     'use strict';
-    GM_addStyle(".whisper-yizhibo{position:absolute;right:50px;top:8px;color:#fff;z-index:9999;padding:0px 4px;text-align:center;border-radius:5px;background:none;cursor:default;font-size:25px;}.whisper-yizhibo:hover{color:white;background:#880000;cursor:pointer;}");
+    GM_addStyle(".whisper-translate{position:fixed;left:0;top:150px;color:#fff;z-index:9999;width:18px;line-height:30px;text-align:center;border-radius:0px 5px 5px 0px;background:#666;cursor:default;font-size:15px;}.whisper-translate:hover{color:white;background:#880000;cursor:pointer;}");
 
     Page.entry();
 })();
